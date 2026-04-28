39fbdecbffe556ec5d5d3dbd0a0b5ca7f60c7725
lassilos <bienentv2007@gmail.com>
2026-04-28 23:35:02 +0200

    First Neoforge Version, Basic Items are Implemented Missing: - Right click behavior for Rocket boosters - Recipes, mainly Stone Casing and other Recipes previously done through CreateTweaker - Villager Variants

---
+++ src/main/java/org/lassilos/create_skyblock_addon/item/RocketboosterItem.java
@@ -0,0 +1,45 @@
+
+package org.lassilos.create_skyblock_addon.item;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionHand;
+
+import org.lassilos.create_skyblock_addon.procedures.RocketboosterRightclickedProcedure;
+
+public class RocketboosterItem extends Item {
+	public RocketboosterItem() {
+		super(new Item.Properties().stacksTo(64).rarity(Rarity.UNCOMMON));
+	}
+
+	@Override
+	public UseAnim getUseAnimation(ItemStack itemstack) {
+		return UseAnim.SPEAR;
+	}
+
+	@Override
+	public int getEnchantmentValue() {
+		return -1;
+	}
+
+	@Override
+	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
+		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
+		RocketboosterRightclickedProcedure.execute(world, entity.getX(), entity.getY(), entity.getZ(), entity);
+		return ar;
+	}
+
+	@Override
+	public InteractionResult useOn(UseOnContext context) {
+		super.useOn(context);
+		RocketboosterRightclickedProcedure.execute(context.getLevel(), context.getClickedPos().getX(), context.getClickedPos().getY(), context.getClickedPos().getZ(), context.getPlayer());
+		return InteractionResult.SUCCESS;
+	}
+}
